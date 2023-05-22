e738c2019fd834e9465906beea1a99465a8f0ec3
Hugo Barbosa Rodrigues <hugobrod72@gmail.com>
2023-05-22 15:06:58 -0300

    Express generator

---
+++ routes/produtos.js
@@ -0,0 +1,45 @@
+var express = require('express');
+var router = express.Router();
+const fs = require('fs')
+
+const produtos = require("../public/Json/produtos.json")
+
+/* GET users listing. */
+router.get('/', function (req, res, next) {
+
+  res.send("Get na raiz do cominho produtos");
+
+  //if (!req.query.TOKEN) {
+  //res.status(400).send("Usuário sem Token de acesso ou inválido!")
+  // return
+  //}
+  //res.status(200).json(produtos)
+});
+
+router.post('/add', function (req, res, next) {
+
+  res.send("Post do caminho produtos/add, seria para adicionar um produto!");
+
+  // if (!req.query.TOKEN) {
+  //   res.status(400).send("Usuário sem Token de acesso ou inválido!")
+  //   return
+  // }
+
+  // if (!req.body) {
+  //   res.status(400).send("Sem valores para serem adicionados!")
+  //   return
+  // }
+  // const resProdutos = {
+  //   ...produtos, 7: {
+  //     id: 8,
+  //     name: req.body.name,
+  //     description: req.body.description,
+  //     price: req.body.price
+  //   }
+  // }
+
+  // res.status(200).json(resProdutos)
+
+})
+
+module.exports = router;
